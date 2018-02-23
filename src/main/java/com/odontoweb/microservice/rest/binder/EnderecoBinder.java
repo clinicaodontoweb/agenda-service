0fb787873c221018e77c4275ba0c30ae6b1ac094
fernandorosa <generoso.fernando@gmail.com>
2018-02-23 01:54:35 -0300

    formatação de código

--- src/main/java/com/odontoweb/microservice/rest/binder/EnderecoBinder.java
+++ src/main/java/com/odontoweb/microservice/rest/binder/EnderecoBinder.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import com.odontoweb.microservice.impl.model.Cep;
 import com.odontoweb.microservice.impl.model.Endereco;
 import com.odontoweb.microservice.impl.service.BairroService;
 import com.odontoweb.microservice.impl.service.CepService;
